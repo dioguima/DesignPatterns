0ae9cfe5c0303483fbb6bae598db2fb5a576d05a
Diogo Guimarães <diogoferreiraguima@hotmail.com>
2018-10-04 07:36:26 -0300

    Added singleton pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reference: https://sourcemaking.com/design_patterns/singleton
+            Singleton singleton = Singleton.Instance;
+            Console.WriteLine($"Initial name: {singleton.Name}");
+            singleton.Name = "Jane Doe";
+            Console.WriteLine($"Changed name: {singleton.Name}");
+            Singleton anotherSingleton = Singleton.Instance;
+            Console.WriteLine($"Name in another variable: {anotherSingleton.Name}");
+            Console.Read();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        public string Name { get; set; }
+
+        private Singleton()
+        {
+            this.Name = "John Doe";
+        }
+
+    }
+}
