f6a43d8f333edf6654b97d6ef15f950f2eeaa30a
dioguima <diogoferreiraguima@hotmail.com>
2018-10-04 21:11:26 -0300

    Visitor pattern implemented

---
+++ Behavioral/Visitor/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Visitor
+{
+    class Circle
+    {
+        private int x;
+        private int y;
+        private int radius;
+        
+        public Circle(int x, int y, int radius)
+        {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        public bool IsColliding(Circle circle)
+        {
+            double diff = Math.Sqrt(Math.Pow(circle.x - this.x, 2) + Math.Pow(circle.y - this.y, 2));
+            return diff <= circle.radius + this.radius;
+        }
+    }
+}
---
+++ Behavioral/Visitor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reference: https://sourcemaking.com/design_patterns/visitor
+            Circle circleA = new Circle(5, 5, 5);
+            Circle circleB = new Circle(15, 5, 5);
+            Circle circleC = new Circle(16, 11, 5);
+
+            //This circle should be colliding in the bottom
+            Console.WriteLine(circleA.IsColliding(circleB));
+
+            //This circle should not be colliding with any other
+            Console.WriteLine(circleA.IsColliding(circleC));
+
+            Console.Read();
+        }
+    }
+}
