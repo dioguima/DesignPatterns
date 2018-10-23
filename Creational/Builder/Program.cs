059cd4713f9b4fb40ab71efbab219072bb20ef3c
Diogo Guimarães <diogoferreiraguima@hotmail.com>
2018-10-23 09:54:46 -0300

    Builder reference added

--- Creational/Builder/Program.cs
+++ Creational/Builder/Program.cs
@@ -6,6 +6,7 @@ namespace Builder
     {
         static void Main(string[] args)
         {
+            //Reference: https://sourcemaking.com/design_patterns/builder
             EnemyBuilder normalEnemyBuilder = new EnemyBuilder().Name("Bad Guy").LifePoints(100).Type(EEnemyType.Normal).Position(new Tuple<int, int>(10, 10));
             EnemyBuilder bigEnemyBuilder = new EnemyBuilder().Name("Big Bad Guy").LifePoints(300).Type(EEnemyType.Big).Position(new Tuple<int, int>(10, 10));
             Enemy normalEnemy = normalEnemyBuilder.Build();
